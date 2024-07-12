770af3a6222ddefd4862f294001f50894986173e
givemethatsewon <nvpz1598@cau.ac.kr>
2024-07-12 13:58:39 +0900

    성적 저장 애플리케이션 API

---
+++ src/main/java/com/jyujyu/dayonetest/controller/ScoreApi.java
@@ -0,0 +1,44 @@
+package com.jyujyu.dayonetest.controller;
+
+import com.jyujyu.dayonetest.controller.requset.SaveExamScoreRequest;
+import com.jyujyu.dayonetest.controller.response.ExamFailStudentResponse;
+import com.jyujyu.dayonetest.controller.response.ExamPassStudentResponse;
+import com.jyujyu.dayonetest.service.StudentScoreService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ScoreApi {
+    private final StudentScoreService studentScoreService;
+
+    @PutMapping("/exam/{exam}/score")
+    public void save(
+            @PathVariable("exam") String exam,
+            @RequestBody SaveExamScoreRequest request
+            ) {
+        studentScoreService.saveScore(
+                request.getStudentName(),
+                exam,
+                request.getKoScore(),
+                request.getEnglishScore(),
+                request.getMathScore());
+    }
+
+    @GetMapping("/exam/{exam}/pass")
+    public List<ExamPassStudentResponse> pass(
+            @PathVariable("exam") String exam
+    ) {
+            return studentScoreService.getPassStudentList(exam);
+    }
+
+    @GetMapping("/exam/{exam}/fail")
+    public List<ExamFailStudentResponse> fail(
+            @PathVariable("exam") String exam
+    ) {
+        return studentScoreService.getFailStudentList(exam);
+    }
+
+}
---
+++ src/main/java/com/jyujyu/dayonetest/service/StudentScoreService.java
@@ -0,0 +1,78 @@
+package com.jyujyu.dayonetest.service;
+
+import com.jyujyu.dayonetest.MyCalculator;
+import com.jyujyu.dayonetest.controller.response.ExamFailStudentResponse;
+import com.jyujyu.dayonetest.controller.response.ExamPassStudentResponse;
+import com.jyujyu.dayonetest.model.StudentFail;
+import com.jyujyu.dayonetest.model.StudentPass;
+import com.jyujyu.dayonetest.model.StudentScore;
+import com.jyujyu.dayonetest.repository.StudentFailRepository;
+import com.jyujyu.dayonetest.repository.StudentPassRepository;
+import com.jyujyu.dayonetest.repository.StudentScoreRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class StudentScoreService {
+    private final StudentScoreRepository studentScoreRepository;
+    private final StudentFailRepository studentFailRepository;
+    private final StudentPassRepository studentPassRepository;
+
+    public void saveScore(String studentName, String exam, Integer korScore, Integer engScore, Integer mathScore) {
+        StudentScore studentScore = StudentScore.builder()
+                .exam(exam)
+                .studentName(studentName)
+                .korScore(korScore)
+                .engScore(engScore)
+                .mathScore(mathScore)
+                .build();
+
+        studentScoreRepository.save(studentScore);
+
+        MyCalculator calculator = new MyCalculator(0.0);
+        Double avgScore = calculator
+                .add(korScore.doubleValue())
+                .add(engScore.doubleValue())
+                .add(engScore.doubleValue())
+                .divide(3.0)
+                .getResult();
+
+        if (avgScore >= 60) {
+            StudentPass studentPass = StudentPass.builder()
+                    .exam(exam)
+                    .studentName(studentName)
+                    .avgScore(avgScore)
+                    .build();
+            studentPassRepository.save(studentPass);
+        } else {
+            StudentFail studentFail = StudentFail.builder()
+                    .exam(exam)
+                    .studentName(studentName)
+                    .avgScore(avgScore)
+                    .build();
+            studentFailRepository.save(studentFail);
+        }
+    }
+
+    public List<ExamPassStudentResponse> getPassStudentList(String exam) {
+        List<StudentPass> studentPasses = studentPassRepository.findAll();
+
+        return studentPasses.stream()
+                .filter((pass) -> pass.getExam().equals(exam))
+                .map((pass) -> new ExamPassStudentResponse(pass.getStudentName(), pass.getAvgScore()))
+                .toList();
+    }
+
+    public List<ExamFailStudentResponse> getFailStudentList(String exam) {
+        List<StudentFail> studentFails = studentFailRepository.findAll();
+
+        return studentFails.stream()
+                .filter((fail) -> fail.getExam().equals(exam))
+                .map((fail) -> new ExamFailStudentResponse(fail.getStudentName(), fail.getAvgScore()))
+                .toList();
+    }
+
+}
