0f5dbc98ce361348ae2d2c111e49eea6e2dffa1c
givemethatsewon <nvpz1598@cau.ac.kr>
2024-07-15 19:14:45 +0900

    성적 저장 로직 검증 테스트 코드 작성

--- src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
+++ src/test/java/com/jyujyu/dayonetest/service/StudentScoreServiceMockTest.java
@@ -3,7 +3,9 @@ package com.jyujyu.dayonetest.service;
 import com.jyujyu.dayonetest.repository.StudentFailRepository;
 import com.jyujyu.dayonetest.repository.StudentPassRepository;
 import com.jyujyu.dayonetest.repository.StudentScoreRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 
 public class StudentScoreServiceMockTest {
@@ -25,13 +27,82 @@ public class StudentScoreServiceMockTest {
         // when
         studentScoreService.saveScore(
                 givenStudentName,
-                 givenExam,
+                givenExam,
                 givenKorScore,
                 givenEnglishScore,
                 givenMathScore
         );
 
-
-
     }
+
+    @Test
+    @DisplayName("성적 저장 로직 검증 / 60점 이상인 경우")
+    public void saveScoreMockTest1() {
+        // given
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
+
+        StudentScoreService studentScoreService = new StudentScoreService(
+                studentScoreRepository,
+                studentFailRepository,
+                studentPassRepository
+        );
+
+        String givenStudentName = "testName";
+        String givenExam = "testExam";
+        Integer givenKorScore = 100;
+        Integer givenEnglishScore = 100;
+        Integer givenMathScore = 60;
+
+        // when
+        studentScoreService.saveScore(
+                givenStudentName,
+                givenExam,
+                givenKorScore,
+                givenEnglishScore,
+                givenMathScore
+        );
+
+        // then
+        Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(studentPassRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(studentFailRepository, Mockito.times(0)).save(Mockito.any());
+    }
+
+    @Test
+    @DisplayName("성적 저장 로직 검증 / 60점 이하인 경우")
+    public void saveScoreMockTest2() {
+        // given
+        StudentScoreRepository studentScoreRepository = Mockito.mock(StudentScoreRepository.class);
+        StudentPassRepository studentPassRepository = Mockito.mock(StudentPassRepository.class);
+        StudentFailRepository studentFailRepository = Mockito.mock(StudentFailRepository.class);
+
+        StudentScoreService studentScoreService = new StudentScoreService(
+                studentScoreRepository,
+                studentFailRepository,
+                studentPassRepository
+        );
+
+        String givenStudentName = "testName";
+        String givenExam = "testExam";
+        Integer givenKorScore = 50;
+        Integer givenEnglishScore = 50;
+        Integer givenMathScore = 50;
+
+        // when
+        studentScoreService.saveScore(
+                givenStudentName,
+                givenExam,
+                givenKorScore,
+                givenEnglishScore,
+                givenMathScore
+        );
+
+        // then
+        Mockito.verify(studentScoreRepository, Mockito.times(1)).save(Mockito.any());
+        Mockito.verify(studentPassRepository, Mockito.times(0)).save(Mockito.any());
+        Mockito.verify(studentFailRepository, Mockito.times(1)).save(Mockito.any());
+    }
+
 }
